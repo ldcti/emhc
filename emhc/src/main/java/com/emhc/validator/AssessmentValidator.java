81991ed94cda43a461a1963770b32e5dc4653806
dong <ldcti@hotmail.com>
2017-12-19 08:54:16 -0500

    assessment validation reflection implement.

--- emhc/src/main/java/com/emhc/validator/AssessmentValidator.java
+++ emhc/src/main/java/com/emhc/validator/AssessmentValidator.java
@@ -33,35 +33,83 @@ public class AssessmentValidator implements Validator {
 
 	private void validateAnswerDTO(Errors errors, AnswerDTO form) {
 
+/*		try {
+			Class<?> c = Class.forName(getClassName());
+			Object t = c.newInstance();
+
+			Method[] allMethods = c.getDeclaredMethods();
+			for (Method m : allMethods) {
+				String mname = m.getName();
+				if (!mname.startsWith("getButton") || (m.getGenericReturnType() != boolean.class)) {
+					continue;
+				}
+				Type[] pType = m.getGenericParameterTypes();
+				if ((pType.length != 1) || Locale.class.isAssignableFrom(pType[0].getClass())) {
+					continue;
+				}
+
+				out.format("invoking %s()%n", mname);
+				try {
+					m.setAccessible(true);
+					Object o = m.invoke(t, new Locale(args[1], args[2], args[3]));
+					out.format("%s() returned %b%n", mname, (Boolean) o);
+
+					// Handle any exceptions thrown by method to be invoked.
+				} catch (InvocationTargetException x) {
+					Throwable cause = x.getCause();
+					err.format("invocation of %s failed: %s%n", mname, cause.getMessage());
+				}
+			}
+
+			// production code should handle these exceptions more gracefully
+		} catch (ClassNotFoundException x) {
+			x.printStackTrace();
+		} catch (InstantiationException x) {
+			x.printStackTrace();
+		} catch (IllegalAccessException x) {
+			x.printStackTrace();
+		}
+	}*/
 		try {
 			Class<?> c = Class.forName(getClassName());
 			Method[] methods = c.getMethods();
-			Object object = c.newInstance();
-
+//			AnswerDTO object = c.newInstance();
+			String name;
 			for (int i = 0; i < methods.length; i++) {
 				if (methods[i].getName().startsWith("getButton")) {
 					System.out.println(methods[i].getName());
 					Method m = methods[i];
-					if (m.invoke(object) == null) {
-						errors.rejectValue("button01", "Assessment.select.error");
+					try{
+					m.setAccessible(true);
+					if (m.invoke(form) == null) {
+						name = methods[i].getName();
+						name.replace("", "getButton");
+						String temp = "Assessment.select.error" + name;
+						errors.rejectValue("button01", temp);
 
-					} else if (methods[i].invoke(object).equals("Yes")) {
+					} else if (methods[i].invoke(form).equals("Yes")) {
 
-						String name = methods[i].getName();
+						name = methods[i].getName();
 						name.replace("Button", "Answer");
 						System.out.println("----------------------" + name);
 						Method mm = c.getMethod(name, null);
-						if (mm.invoke(object).equals(null)) {
+						if (mm.invoke(form).equals(null)) {
 							LOGGER.debug("email not in right format");
 							errors.rejectValue("button01", "Assessment.selected.error");
 
 						}
-
 					}
-
+					}
+					catch (InvocationTargetException x) {
+					    Throwable cause = x.getCause();
+					}
 				}
+				}
+				
+			  } catch (Exception ex) {
+			ex.printStackTrace();
 			}
-
+			
 			/*
 			 * if (form.getButton01() == null) { errors.rejectValue("button01",
 			 * "Assessment.select.error");
@@ -81,9 +129,7 @@ public class AssessmentValidator implements Validator {
 			 * errors.rejectValue("button01", "Assessment.selected.error");
 			 * 
 			 * } }
-			 */ } catch (Exception ex) {
-			ex.printStackTrace();
-		}
+		}*/
 
 	}
 
